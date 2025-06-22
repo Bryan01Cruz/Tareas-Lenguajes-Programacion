3f9fa2b8ea9f8f17d3957ac09192c8b90c8d4202
Bryan Cruz <bcruzi@unah.hn>
2025-06-22 11:14:43 -0600

    Creación del Column principal y el título de la pantalla.

--- tarea_pantalla/lib/main.dart
+++ tarea_pantalla/lib/main.dart
@@ -16,7 +16,20 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+
+            children: [
+              Text('Welcome to', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),),
+              Text('Reminders', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),),
+              SizedBox(height: 15,),
+              
+            ],
+          ),
+        ),
       ),
     );
   }
