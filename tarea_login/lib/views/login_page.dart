7225143309a326ebb2457e2aefde2ac1b666da74
Bryan Cruz <bcruzi@unah.hn>
2025-07-12 20:52:28 -0600

    Construyendo la pagina de login_page

--- tarea_login/lib/views/login_page.dart
+++ tarea_login/lib/views/login_page.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+//import 'package:go_router/go_router.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  //final ControlUsuario = TextEditingController();
+  //final ControlContraseña = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // drawer: Drawer(),
-      appBar: AppBar(title: Text('Inicio de sesión')),
-      body: Center(child: Text('Login Page')),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Inicio de sesión', style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),)
+              ],
+            ),
+          ),
+          ),
+      ),
     );
   }
 }
